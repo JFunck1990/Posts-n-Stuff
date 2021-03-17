adf0a4dae5f685cb7b037015037fa97919611a99
Jack Funck <jackfunck21@gmail.com>
2021-03-17 18:15:06 -0400

    this works

--- public/assets/static/script.js
+++ public/assets/static/script.js
@@ -1,5 +1,6 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 // eslint-disable-next-line no-unused-vars
+
 function openNav () {
   document.getElementById('mySidebar').style.width = '250px';
   document.getElementById('main').style.marginLeft = '250px';
